9e055566e3a903af9e0294a7e0f218f3b0b3d304
xeth.hung@HTGVMS250821-1 <adamhung12@gmail.com>
2026-06-28 19:13:33 +0800

    updates

--- src/basic_app_frame_xethhung12/connectors/s3.py
+++ src/basic_app_frame_xethhung12/connectors/s3.py
@@ -36,7 +36,6 @@ class S3Context:
         self.session = boto3.Session(
             aws_access_key_id=self.aws_access_key_id,
             aws_secret_access_key=self.aws_secret_access_key,
-            aws_session_token=self.aws_session_token,
             region_name=self.region_name,
         )
 
