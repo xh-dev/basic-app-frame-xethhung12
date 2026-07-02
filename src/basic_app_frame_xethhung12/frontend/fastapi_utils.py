3164df32d46e66364fee0f90e9204183fb018073
xeth.hung@HTGVMS250821-1 <adamhung12@gmail.com>
2026-07-03 07:40:53 +0800

    updates

--- src/basic_app_frame_xethhung12/frontend/fastapi_utils.py
+++ src/basic_app_frame_xethhung12/frontend/fastapi_utils.py
@@ -3,7 +3,7 @@ from starlette.types import ASGIApp
 
 
 class DarkHoleMiddleware:
-    def __init__(self, app: ASGIApp, expected_key: str, apikey_handler=default_apikey_handler):
+    def __init__(self, app: ASGIApp, expected_key: str, apikey_handler=DarkHoleMiddleware.default_apikey_handler):
         self.app = app
         self.expected_key = expected_key
         self.apikey_handler = apikey_handler
@@ -25,7 +25,5 @@ class DarkHoleMiddleware:
             return True
 
 def apply_dark_host_middleware(app: FastAPI, expected_key: str, apikey_handler=None):
-    if apikey_handler is None:
-        apikey_handler = DarkHoleMiddleware.default_apikey_handler
     app.add_middleware(DarkHoleMiddleware, expected_key=expected_key, apikey_handler=apikey_handler)
 
