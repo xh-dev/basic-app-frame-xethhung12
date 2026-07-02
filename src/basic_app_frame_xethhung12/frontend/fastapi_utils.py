2b2981537bf24e70ee00c452909509bf2377b220
xeth.hung@HTGVMS250821-1 <adamhung12@gmail.com>
2026-07-03 07:29:34 +0800

    add darkhole

---
+++ src/basic_app_frame_xethhung12/frontend/fastapi_utils.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+from startle.types import ASGIApp
+
+
+class DarkHoleMiddleware:
+    def __init__(self, app: ASGIApp, expected_key: str, apikey_handler=default_apikey_handler):
+        self.app = app
+        self.expected_key = expected_key
+        self.apikey_handler = apikey_handler
+
+    async def __call__(self, scope, receive, send):
+        if scope["type"] == "http":
+            request = Request(scope, receive)
+            if not self.apikey_handler(request, self.expected_key):
+                await send({"type": "http.discount"})
+                return
+        await self.app(scope, receive, send)
+    
+    @staticmethod
+    def default_apikey_handler(request: Request, expected_key: str):
+        key = request.get("X-Dark-Host-Key")
+        if not key or key != expected_key:
+            return False
+        else:
+            return True
+
+def apply_dark_host_middleware(app: FastAPI, expected_key: str, apikey_handler=None):
+    if apikey_handler is None:
+        apikey_handler = DarkHoleMiddleware.default_apikey_handler
+    app.add_middleware(DarkHoleMiddleware, expected_key=expected_key, apikey_handler=apikey_handler)
+
