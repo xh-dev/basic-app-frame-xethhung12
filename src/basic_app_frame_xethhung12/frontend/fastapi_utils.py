4ddac78cb5b162061515344809a18ff95cbb5771
xeth.hung@HTGVMS250821-1 <adamhung12@gmail.com>
2026-07-03 07:44:34 +0800

    updates

--- src/basic_app_frame_xethhung12/frontend/fastapi_utils.py
+++ src/basic_app_frame_xethhung12/frontend/fastapi_utils.py
@@ -2,17 +2,21 @@ from fastapi import FastAPI, Request
 from starlette.types import ASGIApp
 
 
+def _default_apikey_handler(request: Request, expected_key: str):
+    key = request.get("X-Dark-Hole-Key")
+    if not key or key != expected_key:
+        return False
+    else:
+        return True
 class DarkHoleMiddleware:
     @staticmethod
     def default_apikey_handler(request: Request, expected_key: str):
-        key = request.get("X-Dark-Hole-Key")
-        if not key or key != expected_key:
-            return False
-        else:
-            return True
+        return _default_apikey_handler(request, expected_key)
 
-    def __init__(self, app: ASGIApp, expected_key: str, apikey_handler=DarkHoleMiddleware.default_apikey_handler):
+    def __init__(self, app: ASGIApp, expected_key: str, apikey_handler):
         self.app = app
+        if apikey_handler is None:
+            apikey_handler = _default_apikey_handler
         self.expected_key = expected_key
         self.apikey_handler = apikey_handler
 
